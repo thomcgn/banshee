07b8a651e027b8c57c3adc7bf9b23d2fbbe2fb5e
thomcgn <kramer.th@outlook.de>
2025-12-16 17:37:53 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/dist", // <- Hier sicherstellen, dass die Assets im Root erreichbar sind
   plugins: [react()],
   server: {
     port: 5173,
